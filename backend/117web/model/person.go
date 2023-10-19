c4418b73569fbd2bc6460aaf51ca362c74de80ca
Fu zhicheng <fuzhicheng@guanxingtuan.top>
2023-10-19 22:28:23 +0800

    feat.重新规划

--- backend/117web/model/person.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Person struct {
-	RealName string `json:"realname",grom:"size:64"`
-	Uid      int    `json:"uid",grom:"uid"`
-	Gender   string `json:"gender",gorm:"size:8"`
-}
--- backend/117web/model/user.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type User struct {
-	UserName string `json:"username",gorm:"size:255"`
-	PassWord string `json:"password",gorm:"size:255"`
-}
